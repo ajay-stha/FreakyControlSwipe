47563e3a983e8b1294916483a1658d34b6dfba60
ajay-stha <ajay.shrestha54@gmail.com>
2025-05-18 14:34:58 +0545

    Update UI components and manage animations
    - Removed `scanning.json` from project assets.
    - Added SkiaSharp namespace and new UI elements in `MainPage.xaml`, including `SKLottieView` and `CollectionView`.
    - Introduced `ObservableCollection<CollectionDataItem>` in `MainPage.xaml.cs` for dynamic item management.
    - Updated `MauiProgram.cs` to include SkiaSharp library.
    - Created `CollectionDataItem` class for item properties.
    - Added new `scanning.json` for animation data.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,12 +1,25 @@
-﻿namespace FreakyControlsSample
+﻿using FreakyControlsSample.DTO;
+using System.Collections.ObjectModel;
+
+namespace FreakyControlsSample
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
+        public ObservableCollection<CollectionDataItem> Items { get; set; } = new()
+            {
+                new CollectionDataItem { Name = "Item 1", IsVisible = false },
+                new CollectionDataItem { Name = "Item 2", IsVisible = true },
+                new CollectionDataItem { Name = "Item 3", IsVisible = true },
+                new CollectionDataItem { Name = "Item 4", IsVisible = false },
+                new CollectionDataItem { Name = "Item 5", IsVisible = true },
+                new CollectionDataItem { Name = "Item 6", IsVisible = true },
+            };
 
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
